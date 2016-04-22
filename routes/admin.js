a5569f397d1e70971df21a24c97ce6128e711d00
jimxiang <jxhzr94151@gmail.com>
2016-04-22 17:35:20 +0800

    admin login

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,14 @@
  */
 var express = require('express');
 var router = express.Router();
+var admin = require('../lib/admin/user');
 var wilddog = require('wilddog');
 var ref = new wilddog('https://wild-boar-00060.wilddogio.com/');
-/* GET users listing. */
+/* GET admin listing. */
 router.get('/', function(req, res, next) {
     res.send('here is admin');
 });
-/* GET/POST lister*/
-// router.get('/login',function (req,res,next) {
-//   //code here
-// })
+
+router.post('/back/login', admin.login);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,16 +3,17 @@ var router = express.Router();
 var test = require('./user/ExportFunctionDemo');
 var wilddog = require('wilddog');
 var ref = new wilddog('https://wild-boar-00060.wilddogio.com/');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  ref.once('value',(snap)=>{
-    console.log(snap.val());
-  });
-  test.a();
-  test.b(function(){
-    console.log('this is callback function');
-  });
-  res.send('here is user');
+  	ref.once('value',(snap)=>{
+    	console.log(snap.val());
+  	});
+   
+  	test.b(function(){
+    	console.log('this is callback function');
+  	});
+  	res.send('here is user');
 });
 /* GET/POST lister*/
 // router.get('/login',function (req,res,next) {
