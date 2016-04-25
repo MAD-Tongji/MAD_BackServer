19e64950dc4416aef8302a830e7e75c6247f230d
Teng <843018739@qq.com>
2016-04-25 22:22:41 +0800

    init advertiser

--- routes/advertiser.js
+++ routes/advertiser.js
@@ -7,8 +7,116 @@ router.get('/', function(req, res, next) {
   res.send('here is advertiser');
 });
 /* GET/POST lister*/
-// router.get('/login',function (req,res,next) {
-//   //code here
-// })
+
+
+/************** 登陆注册部分 ***********/
+// 登录
+router.post('/login',function (req,res,next) {
+  //code here
+});
+
+// 注册
+router.post('/signup',function (req,res,next) {
+  //code here
+});
+
+
+/************** 广告部分 ***********/
+// 查看已发布广告列表
+router.get('/advertisement/list/all',function (req,res,next) {
+  //code here
+});
+
+// 获取投放商圈列表
+router.get('/advertisement/district/all',function (req,res,next) {
+  //code here
+});
+
+// 发布新广告
+router.post('/advertisement/submit',function (req,res,next) {
+  //code here
+});
+
+// 将广告保存为草稿
+router.post('/advertisement/save',function (req,res,next) {
+  //code here
+});
+
+// 根据ID下架广告
+router.post('/advertisement/remove',function (req,res,next) {
+  //code here
+});
+
+// 根据ID获取广告详情(这个接口好像设计的有问题，我还不知道怎么实现，需要查一查)
+router.get('/advertisement/',function (req,res,next) {
+  //code here
+});
+
+
+/************** 广告商账户部分 ***********/
+// 账户充值
+router.post('/account/recharge',function (req,res,next) {
+  //code here
+});
+
+// 获取广告商账户信息
+router.get('/account/detail',function (req,res,next) {
+  //code here
+});
+
+// 获取充值记录
+router.get('/account/recharge/all',function (req,res,next) {
+  //code here
+});
+
+// 获取退款记录
+router.get('/account/refund/all',function (req,res,next) {
+  //code here
+});
+
+// 申请退款
+router.post('/account/refund',function (req,res,next) {
+  //code here
+});
+
+// 提交验证信息
+router.post('/account/check',function (req,res,next) {
+  //code here
+});
+
+// 上传营业执照图片
+router.post('/account/license',function (req,res,next) {
+  //code here
+});
+
+
+
+/************** 统计部分 ***********/
+// 获取广告统计列表
+router.get('/statistics/all',function (req,res,next) {
+  //code here
+});
+
+// 根据ID获取广告支出详情
+router.get('/statistics/detail',function (req,res,next) {
+  //code here
+});
+
+// 获取广告商支出和投放数据
+router.get('/statistics/data',function (req,res,next) {
+  //code here
+});
+
+//消息部分
+// 获取消息列表
+router.get('/message',function (req,res,next) {
+  //code here
+});
+
+/************** 杂项 ***********/
+// 查询服务器当前时间
+router.get('/time',function (req,res,next) {
+  //code here
+});
 
 module.exports = router;
