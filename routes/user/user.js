55f2c1b1a8ae8022a6569c5ee117903335624225
Tokkiu <mrgao.ary@gmail.com>
2016-04-25 23:53:11 +0800

    添加methods和user2个文件分别用于提供方法和数据库操作，修改user里的路由

---
+++ routes/user/user.js
@@ -0,0 +1,28 @@
+var crypto=require('crypto');
+//var redis=require('redis');
+var Token=require('../../lib/publicUtils');
+var Q=require('q');
+//var db=redis.createClient();
+var wilddog=require('wilddog');
+
+module.exports=User;
+
+function User(params) {
+    
+}
+
+User.login=function(data,fn) {
+    
+}
+
+User.register=function(data,fn) {
+    
+}
+
+User.findpwd=function(data,fn) {
+    
+}
+
+User.alterpwd=function(data.fn) {
+    
+}
