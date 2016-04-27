15501c6ae849fe86fe126d2c854edd55789f53ca
Tokkiu <mrgao.ary@gmail.com>
2016-04-27 10:52:46 +0800

    1.获取消息列表接口跑通 2.完善注册时信息

--- routes/user.js
+++ routes/user.js
@@ -35,7 +35,7 @@ router.post('/findpwd',access.findpwd);
 router.post('/alterpwd',access.alterpwd);
 
 /* user get message list interface */
- router.post('/msglist',access.msglist);
+ router.post('/msglist/:userid',access.msglist);
 
 /* user get ad-used list interface */
 router.get('/advert/all/:userid',advert.getAllAdUsed);
